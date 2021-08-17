b3fc6155f97f5ce2b54322a3932ed7d9107c7127
Ella Choy <egal21de@gmail.com>
2021-08-17 20:45:31 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,7 @@ console.log("test")
       bis 120 x 60 x 60 cm
 // 3
 "<b>Extra große Größe</b> */
-
+// default
 
 function showtxt() {
     let list = document.getElementById("mylist").value
@@ -24,7 +24,7 @@ function showtxt() {
 
     switch (list) {
         case ("0"):
-            document.getElementById("masse").innerHTML = "<b>Brief und Postkarte</b> <br> L: 10 - 23,5 cm B: 7 - 12,5 cm H: bis 1 cm"
+            document.getElementById("masse").innerHTML = "<b>Brief und Postkarte</b> <br> L: 10 - 23,5 cm <br>B: 7 - 12,5 cm <br>H: bis 1 cm"
             break;
         case ("1"):
             document.getElementById("masse").innerHTML = "<b>DHL Paket 2 kg</b> <br> bis 60 x 30 x 15 cm"
